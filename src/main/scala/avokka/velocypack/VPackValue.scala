db8d254bd364b6fea9961650fda3bc6b76930455
bicou <bicou@bicou.com>
2019-11-08 15:32:46 +0100

    wip

--- src/main/scala/avokka/velocypack/VPackValue.scala
+++ src/main/scala/avokka/velocypack/VPackValue.scala
@@ -1,5 +1,8 @@
 package avokka.velocypack
 
+import java.time.Instant
+
+import avokka.velocypack.codecs.between
 import scodec._
 import scodec.bits._
 import scodec.codecs._
@@ -7,35 +10,23 @@ import scodec.codecs._
 sealed trait VPackValue {
 }
 
-sealed trait VPackString extends VPackValue {
-  def value: String
+case object VPackReserved1 extends VPackValue {
+  val byte = hex"15"
+  implicit val codec: Codec[VPackReserved1.type] = constant(byte) ~> provide(VPackReserved1)
 }
 
-object VPackString {
-  val r = Range.inclusive(0x40, 0x45)
-
-  r.contains(0x41)
+case object VPackReserved2 extends VPackValue {
+  val byte = hex"16"
+  implicit val codec: Codec[VPackReserved2.type] = constant(byte) ~> provide(VPackReserved2)
 }
 
-case class VPackStringShort(value: String) extends VPackString {
-
-}
-
-object VPackStringShort {
-  def byteType: Byte = 0x40
-  implicit val codec: Codec[VPackStringShort] = { constant(byteType) :: utf8 }.dropUnits.as[VPackStringShort]
-}
-
-case class VPackStringLong(value: String) extends VPackString {
-}
-
-object VPackStringLong {
-  val byte: Byte = 0xbf.toByte
-  implicit val codec: Codec[VPackStringLong] = { constant(byte) :: utf8_32L }.dropUnits.as[VPackStringLong]
+case object VPackIllegal extends VPackValue {
+  val byte = hex"17"
+  implicit val codec: Codec[VPackIllegal.type] = constant(byte) ~> provide(VPackIllegal)
 }
 
 case object VPackNull extends VPackValue {
-  val byte: Byte = 0x18
+  val byte = hex"18"
   implicit val codec: Codec[VPackNull.type] = constant(byte) ~> provide(VPackNull)
 }
 
@@ -44,39 +35,126 @@ sealed trait VPackBoolean extends VPackValue {
 }
 
 case object VPackFalse extends VPackBoolean {
-  val byte: Byte = 0x19
+  val byte = hex"19"
   override val value = false
   implicit val codec: Codec[VPackFalse.type] = constant(byte) ~> provide(VPackFalse)
 }
 
 case object VPackTrue extends VPackBoolean {
-  val byte: Byte = 0x1a
+  val byte = hex"1a"
   override val value = true
   implicit val codec: Codec[VPackTrue.type] = constant(byte) ~> provide(VPackTrue)
+}
+
+case class VPackDouble(value: Double) extends VPackValue
+
+object VPackDouble {
+  val byte = hex"1b"
+  implicit val codec: Codec[VPackDouble] = { constant(byte) :: doubleL }.dropUnits.as
+}
+
+case class VPackInstant(value: Instant) extends VPackValue
+
+object VPackInstant {
+  val byte = hex"1c"
+  val instantCodec: Codec[Instant] = int64L.xmap(Instant.ofEpochMilli,_.toEpochMilli)
+  implicit val codec: Codec[VPackInstant] = { constant(byte) :: instantCodec }.dropUnits.as
+}
+
+case object VPackExternal extends VPackValue {
+  val byte = hex"1d"
+  implicit val codec: Codec[VPackExternal.type] = constant(byte) ~> provide(VPackExternal)
+}
+
+case object VPackMinKey extends VPackValue {
+  val byte = hex"1e"
+  implicit val codec: Codec[VPackMinKey.type] = constant(byte) ~> provide(VPackMinKey)
+}
+
+case object VPackMaxKey extends VPackValue {
+  val byte = hex"1f"
+  implicit val codec: Codec[VPackMaxKey.type] = constant(byte) ~> provide(VPackMaxKey)
+}
+
+/*
+case class VPackInt(value: Int) extends VPackValue
+
+object VPackInt {
+  val byte = hex"20"
+
+//  val c1: Codec[Int] = { constant(hex"20") :: int8L }.dropUnits.as
+
+  implicit val codec: Codec[VPackInt] = {
+    between(uint8L, 0x20, 0x27) >>~ (size => fixedSizeBytes(size + 1, utf8))
+    }.xmap[VPackInt](
+    s => VPackStringShort(s._2),
+    p => (p.value.length, p.value)
+  )
+}
+ */
+
+sealed trait VPackString extends VPackValue {
+  def value: String
+}
+
+object VPackString {
+}
+
+case class VPackStringShort(value: String) extends VPackString {
+  require(value.length <= 126, "VPackStringShort length must be <= 126")
+}
+
+object VPackStringShort {
+  val byte = hex"40"
+
+  implicit val codec: Codec[VPackStringShort] = {
+    between(uint8L, 0x40, 0xbe) >>~ (size => fixedSizeBytes(size, utf8))
+  }.xmap[VPackStringShort](
+    s => VPackStringShort(s._2),
+    p => (p.value.length, p.value)
+  )
+}
+
+case class VPackStringLong(value: String) extends VPackString
+
+object VPackStringLong {
+  val byte = hex"bf"
+  implicit val codec: Codec[VPackStringLong] = { constant(byte) :: utf8_32L }.dropUnits.as
+}
+
+case class VPackBinary(value: ByteVector) extends VPackValue {
+  def lengthSize: Int = {
+    val length = value.size
+    if      (length > 0xffffffffffffffL) 8
+    else if (length > 0xffffffffffffL) 7
+    else if (length > 0xffffffffffL) 6
+    else if (length > 0xffffffffL) 5
+    else if (length > 0xffffffL) 4
+    else if (length > 0xffffL) 3
+    else if (length > 0xffL) 2
+    else 1
+  }
+}
+
+object VPackBinary {
+  implicit val codec: Codec[VPackBinary] = {
+    between(uint8L, 0xc0, 0xc7) >>~ (delta =>
+      variableSizeBytesLong(ulongL((delta + 1) * 8), bytes)
+    )
+  }.xmap[VPackBinary](
+    s => VPackBinary(s._2),
+    p => (p.lengthSize - 1, p.value)
+  )
 }
 
 object VPackValue {
   implicit val codec: Codec[VPackValue] = lazily { Codec.coproduct[VPackValue].choice }
 
-}
 
-/*
-case class VPackNull()
-
-object VPackNull {
-  implicit val codec: Codec[VPackNull] = {
-    constant(hex"18")
-  }.as[VPackNull]
-}
-
-case class VPackString
-(
-  value: String
-) extends VPackValue
-
-object VPackString {
-  implicit val codec: Codec[VPackString] = {
-    ("head" | constant(hex""))
+  def main(args: Array[String]): Unit = {
+    for {
+      e <- codec.encode(VPackBinary(hex"aabb"))
+      d <- codec.decode(e)
+    } yield println(e, d)
   }
 }
-*/
--- src/main/scala/avokka/velocypack/codecs.scala
+++ src/main/scala/avokka/velocypack/codecs.scala
@@ -5,10 +5,10 @@ import scodec.bits._
 import scodec.codecs._
 
 object codecs {
-  import Numeric.Implicits._
-  import Ordering.Implicits._
 
   def between[T : Numeric](codec: Codec[T], min: T, max: T): Codec[T] = {
+    import Numeric.Implicits._
+    import Ordering.Implicits._
     codec.exmap(
       i => if (min <= i && i <= max) Attempt.Successful(i - min) else Attempt.failure(Err("not in range")),
       d => if (d <= max - min) Attempt.Successful(min + d) else Attempt.failure(Err("not in range"))
@@ -16,7 +16,9 @@ object codecs {
   }
 
   def main(args: Array[String]): Unit = {
-    val r40 = between(uint8L, 0x40, 0x45)
+    val r40 = {
+      between(uint8L, 0x40, 0x45)
+    }
 
     println(r40.encode(0))
     println(r40.decode(hex"45".toBitVector))

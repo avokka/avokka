8bba18ced21a6f0b1d98754d48c061c7145e424a
Bicou <bicou@bicou.com>
2019-11-18 22:17:30 +0100

    wip

--- src/main/scala/avokka/velocypack/VelocypackArrayEncoder.scala
+++ src/main/scala/avokka/velocypack/VelocypackArrayEncoder.scala
@@ -1,68 +1,64 @@
 package avokka.velocypack
 
-import scodec.Attempt.Successful
 import scodec._
 import scodec.bits._
 import scodec.codecs._
 import shapeless._
-import shapeless.ops.hlist._
 
-trait VelocypackArrayEncoder[T] {
+trait IsHlistOfEncoders[E <: HList, A <: HList] {
+  def aEncode(e: E, a: A, offset: Long): Attempt[(BitVector, Vector[Long])]
+}
 
+object IsHlistOfEncoders {
+
+  def apply[E <: HList, A <: HList](implicit isListOfEncoders: IsHlistOfEncoders[E, A]): IsHlistOfEncoders[E, A] = isListOfEncoders
+
+  implicit object HNilIsHListOfEncoders extends IsHlistOfEncoders[HNil, HNil] {
+    override def aEncode(i: HNil, a: HNil, offset: Long): Attempt[(BitVector, Vector[Long])] = Attempt.successful((BitVector.empty, Vector.empty))
+  }
+
+  implicit def hconsIsHListOfEncoders[T, E, I <: HList, A <: HList]
+  (implicit
+    ev: IsHlistOfEncoders[I, A],
+    eve: E <:< Encoder[T]
+  ): IsHlistOfEncoders[E :: I, T :: A] = new IsHlistOfEncoders[E :: I, T :: A] {
+    override def aEncode(i: E :: I, a: T :: A, offset: Long): Attempt[(BitVector, Vector[Long])] = {
+      for {
+        rl <- i.head.encode(a.head)
+        rr <- ev.aEncode(i.tail, a.tail, offset + rl.size / 8)
+      } yield (rl ++ rr._1, offset +: rr._2)
+    }
+  }
+
+  def encoder[I <: HList, A <: HList](i: I)(implicit
+                                            ev: IsHlistOfEncoders[I, A]
+  ): Encoder[A] = new Encoder[A] {
+    override def encode(value: A): Attempt[BitVector] = {
+      ev.aEncode(i, value, 0).flatMap {
+        case (bv, offs) => vector(uint8L).encode(offs.map(_.toInt)).map { offsets =>
+          bv ++ offsets
+        }
+      }
+    }
+    override def sizeBound: SizeBound = SizeBound.unknown
+  }
+
+  def main(args: Array[String]): Unit = {
+    val codecs = encoder(int16L :: int8L :: int16L :: HNil)
+    val args = 1 :: 2 :: 3 :: HNil
+
+    println(codecs.encode(args))
+  }
 }
 
 object VelocypackArrayEncoder {
-
-  object ApplyEncoder extends Poly2 {
-    implicit def valueAndCodec[A] = at[A, Codec[A]]((value: A, encoder: Codec[A]) =>
-      encoder.encode(value)
-    )
-  }
-
-  def apply[L <: HList, M <: HList, Z1 <: HList, Z <: HList, TR](l: L)
-  (implicit
-   m: Comapped.Aux[L, Codec, M],
-   zw: ZipWith.Aux[M, L, ApplyEncoder.type, Z],
-   tr: ToTraversable.Aux[Z, Vector, TR]
-  ): Encoder[M] = new Encoder[M] {
-    override def sizeBound: SizeBound = SizeBound.unknown
-
-    override def encode(value: M): Attempt[BitVector] = {
-      val values = value.zipWith(l)(ApplyEncoder).to[Vector]
-      println(values)
-      val valuesAndOffsets = values.foldLeft(BitVector.empty, 0L, Vector.empty[Long]) {
-        case ((accb, accl, acco), Successful(b : BitVector)) => (accb ++ b, accl + b.size / 8, acco :+ accl)
-      }
-      println(valuesAndOffsets)
-      Attempt.successful(hex"10".bits)
-    }
-  }
 
   case class Dat(i1: Int, i2: Int, i3: Int)
 
   def main(args: Array[String]): Unit = {
     val ea = int8 :: int32 :: int8 :: HNil
-    val ca = apply(ea).as[Dat]
-    ca.encode(Dat(1, 2, 3))
-
-    trait folderLP extends Poly2 {
-      implicit def default[T, L <: HList] = at[T, L]((_, acc) => acc)
-    }
-    object folder extends folderLP {
-      implicit def some[T, L <: HList] = at[Some[T], L]((t, acc) => t.get :: acc)
-    }
-
-    trait filterLP extends Poly1 {
-      implicit def any[T] = at[T](_ => HNil)
-    }
-    object filter extends filterLP {
-      implicit def some[T] = at[Some[T]](_.get :: HNil)
-    }
-
-    val test: Option[Int] :: Option[Int] :: Option[Int] :: HNil = Some(1) :: None :: Some(2) :: HNil
-    val filtered = test.flatMap(filter)
-    println(filtered)
-
+    val ca = IsHlistOfEncoders.encoder(ea).as[Dat]
+    println(ca.encode(Dat(1, 2, 3)))
 
   }
 }

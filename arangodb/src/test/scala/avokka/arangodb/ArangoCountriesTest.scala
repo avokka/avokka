e9736021f8d87b45d2c45b3b2f7eaeb4e8273fc5
Bicou <bicou@bicou.com>
2020-05-04 14:16:57 +0200

    refactor tests

---
+++ arangodb/src/test/scala/avokka/arangodb/ArangoCountriesTest.scala
@@ -0,0 +1,142 @@
+package avokka.arangodb
+
+import api._
+import akka.actor.ActorSystem
+import akka.testkit.TestKit
+import avokka.velocypack.VPack.VObject
+import avokka.velocypack._
+import cats.data.EitherT
+import cats.instances.future._
+import com.dimafeng.testcontainers.ForAllTestContainer
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.flatspec.AsyncFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.OptionValues._
+
+class ArangoCountriesTest
+    extends TestKit(ActorSystem("arangodb-test"))
+    with AsyncFlatSpecLike
+    with Matchers
+    with BeforeAndAfterAll
+    with ForAllTestContainer {
+  import ArangoCountriesTest._
+
+  override val container = ArangodbContainer.Def().start()
+
+  val dbName = DatabaseName("test")
+
+  val session: ArangoSession = new ArangoSession(container.configuration.copy(database = dbName))
+
+  val collName = CollectionName("countries")
+
+  val collection = new ArangoCollection(session.db, collName)
+
+  it should "have test database" in {
+    EitherT(session(DatabaseList())).map { res =>
+      res.header.responseCode should be (200)
+      res.header.`type` should be (MessageType.ResponseFinal)
+      res.body.result should contain (dbName)
+    }.rethrowT
+  }
+
+  it should "have a countries collection" in {
+    EitherT(session.db(CollectionList())).map { res =>
+      res.header.responseCode should be (200)
+      res.header.`type` should be (MessageType.ResponseFinal)
+      res.body.result.map(_.name) should contain (collName)
+    }.rethrowT
+  }
+
+  it should "have france at key FR" in {
+    EitherT(session.db(DocumentRead[Country](DocumentHandle(collName, DocumentKey("FR"))))).map { res =>
+      res.header.responseCode should be (200)
+      res.header.`type` should be (MessageType.ResponseFinal)
+      res.body.name should be ("France")
+    }.rethrowT
+  }
+
+  it should "have france at key FR (collection method)" in {
+    EitherT(session.db(collection.read[Country](DocumentKey("FR")))).map { res =>
+      res.header.responseCode should be (200)
+      res.header.`type` should be (MessageType.ResponseFinal)
+      res.body.name should be ("France")
+    }.rethrowT
+  }
+
+  it should "create, read, update, delete document" in {
+    val key = DocumentKey("XX")
+    (for {
+      c <- EitherT(session.db(collection.create(Country(key, name = "country name"), returnNew = true)))
+      r <- EitherT(session.db(collection.read[Country](key)))
+      u <- EitherT(session.db(collection.update[Country, VObject](key, VObject("name" -> "updated name".toVPack))))
+      d <- EitherT(session.db(collection.remove[Country](key)))
+    } yield {
+      c.header.responseCode should be (202)
+      c.header.`type` should be (MessageType.ResponseFinal)
+      c.body._id should be (collection.handle(key))
+      c.body.`new`.value._rev.repr should not be (empty)
+
+      r.header.responseCode should be (200)
+      r.body._key should be (key)
+
+      u.header.responseCode should be (202)
+      u.body._oldRev.value should be (r.body._rev)
+      u.body._rev should not be (r.body._rev)
+
+      d.header.responseCode should be (202)
+      d.body._rev should be (u.body._rev)
+    }).rethrowT
+  }
+
+  it should "create multiple documents" in {
+    val key1 = DocumentKey("X1")
+    val key2 = DocumentKey("X2")
+    EitherT(session.db(DocumentCreateMulti(collection.name, List(
+      Country(key1, name = "country 1"), Country(key2, name = "country 2")
+    )))).map { res =>
+      res.header.responseCode should be (202)
+      res.header.`type` should be (MessageType.ResponseFinal)
+      res.body.size should be (2)
+      res.body.map(_._key) should contain (key1)
+      res.body.map(_._key) should contain (key2)
+    }.rethrowT
+  }
+
+  it should "query with cursor batch size" in {
+    (for {
+      cursor <- EitherT(session.db(Cursor[VObject, Country](
+        query = "FOR c IN @@col LIMIT @limit RETURN c",
+        bindVars = VObject(
+          "limit" -> 10.toVPack,
+          "@col"  -> collection.name.toVPack
+        ),
+        batchSize = Some(6)
+      )))
+      next <- EitherT(session.db(CursorNext[Country](cursor.body.id.get)))
+    } yield {
+      cursor.header.responseCode should be (201)
+      cursor.body.result.size should be (6)
+      cursor.body.hasMore should be (true)
+
+      next.header.responseCode should be (200)
+      next.body.result.size should be (4)
+      next.body.hasMore should be (false)
+    }).rethrowT
+  }
+
+  override def afterAll(): Unit = {
+    session.closeClient()
+    TestKit.shutdownActorSystem(system)
+  }
+}
+
+object ArangoCountriesTest {
+  case class Country(
+      _key: DocumentKey = DocumentKey.empty,
+      _rev: DocumentRevision = DocumentRevision.empty,
+      name: String
+                    )
+
+  implicit val countryEncoder: VPackEncoder[Country] = VPackRecord[Country].encoder
+  implicit val countryDecoder: VPackDecoder[Country] = VPackRecord[Country].decoder
+}
--- arangodb/src/test/scala/avokka/arangodb/ArangoSessionTest.scala
+++ arangodb/src/test/scala/avokka/arangodb/ArangoSessionTest.scala
@@ -1,11 +1,16 @@
 package avokka.arangodb
 
+import api._
 import akka.actor.ActorSystem
 import akka.testkit.TestKit
+import cats.data.EitherT
+import cats.instances.future._
 import com.dimafeng.testcontainers.ForAllTestContainer
-import org.scalatest.BeforeAndAfterAll
+import org.scalatest.{Assertion, BeforeAndAfterAll}
 import org.scalatest.flatspec.AsyncFlatSpecLike
 import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.Future
 
 class ArangoSessionTest
     extends TestKit(ActorSystem("arangodb-test"))
@@ -17,11 +22,81 @@ class ArangoSessionTest
 
   val session: ArangoSession = new ArangoSession(container.configuration)
 
-  it should "connect" in {
-    session(api.Version()).map { res =>
-      res should be ('right)
-      res.right.get.body.version should be(container.version)
+  it should "get version" in {
+    EitherT(session(Version())).map { res =>
+      res.header.responseCode should be (200)
+      res.body.version should be (container.version)
+    }.rethrowT
+  }
+
+  it should "get version with details" in {
+    EitherT(session(Version(details = true))).map { res =>
+      res.header.responseCode should be (200)
+      res.body.details should not be (empty)
+    }.rethrowT
+  }
+
+  it should "have a _system and test database" in {
+    EitherT(session(DatabaseList())).map { res =>
+      res.header.responseCode should be (200)
+      res.body.result should contain (DatabaseName.system)
+      res.body.result should contain (test.name)
+    }.rethrowT
+  }
+
+  val scratchName = DatabaseName("scratch")
+  val scratch = new ArangoDatabase(session, scratchName)
+
+  it should "create, read and drop a database" in {
+    (for {
+      created <- EitherT(session(DatabaseCreate(scratchName)))
+      listed  <- EitherT(session(DatabaseList()))
+      info    <- EitherT(scratch(DatabaseInfo))
+      dropped <- EitherT(scratch(DatabaseDrop))
+    } yield {
+      created.header.responseCode should be (201)
+      created.body.result should be (true)
+
+      listed.body.result should contain (scratchName)
+
+      info.header.responseCode should be (200)
+      info.body.result.name should be (scratchName)
+
+      dropped.header.responseCode should be(200)
+      dropped.body.result should be (true)
+    }).rethrowT
+  }
+
+  it should "fail creating database with invalid name" in {
+    recoverToExceptionIf[ArangoError.Resp] {
+      EitherT(session(DatabaseCreate(DatabaseName("@")))).rethrowT
+    }.map { e =>
+      e.header.responseCode should be (400)
+      e.error.errorNum should be (1229)
     }
+  }
+
+  val test = new ArangoDatabase(session, DatabaseName("test"))
+  val temp = new ArangoCollection(scratch, CollectionName("temp"))
+
+  it should "create, read and drop a collection" in {
+    (for {
+      created <- EitherT(test(CollectionCreate(temp.name)))
+      listed  <- EitherT(test(CollectionList()))
+      info    <- EitherT(test(CollectionInfo(temp.name)))
+      dropped <- EitherT(test(CollectionDrop(temp.name)))
+    } yield {
+      created.header.responseCode should be (200)
+      created.body.name should be (temp.name)
+
+      listed.body.result.map(_.name) should contain (temp.name)
+
+      info.header.responseCode should be (200)
+      info.body.name should be (temp.name)
+
+      dropped.header.responseCode should be(200)
+      dropped.body.id should be (created.body.id)
+    }).rethrowT
   }
 
   override def afterAll(): Unit = {

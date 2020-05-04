c3676deb5701e964a8d9ac630398661c798e376e
Bicou <bicou@bicou.com>
2020-05-04 14:16:33 +0200

    use docker entrypoint init to import test database

--- arangodb/src/test/scala/avokka/arangodb/ArangodbContainer.scala
+++ arangodb/src/test/scala/avokka/arangodb/ArangodbContainer.scala
@@ -1,11 +1,16 @@
 package avokka.arangodb
 
 import com.dimafeng.testcontainers.GenericContainer
+import org.testcontainers.containers.BindMode
 
 import scala.util.Random
 
-class ArangodbContainer(port: Int, val password: String, val version: String, underlying: GenericContainer)
+class ArangodbContainer(val password: String,
+                        val version: String,
+                        underlying: GenericContainer)
     extends GenericContainer(underlying) {
+
+  import ArangodbContainer._
 
   def configuration: ArangoConfiguration = ArangoConfiguration(
     host = containerIpAddress,
@@ -13,27 +18,33 @@ class ArangodbContainer(port: Int, val password: String, val version: String, un
     username = "root",
     password = password
   )
+
+  def endpoint: String = "http://%s:%d".format(containerIpAddress, mappedPort(port))
 }
 
 object ArangodbContainer {
 
+  val port: Int = 8529
+
   object Defaults {
     val version: String = "3.6.3"
-    val port: Int = 8529
     val password: String = Random.nextLong().toHexString
   }
 
   // In the container definition you need to describe, how your container will be constructed:
-  case class Def(port: Int = Defaults.port, password: String = Defaults.password, version: String = Defaults.version)
+  case class Def(password: String = Defaults.password,
+                 version: String = Defaults.version)
       extends GenericContainer.Def[ArangodbContainer](
         new ArangodbContainer(
-          port,
           password,
           version,
           GenericContainer(
             dockerImage = s"arangodb:$version",
             env = Map("ARANGO_ROOT_PASSWORD" -> password),
             exposedPorts = Seq(port),
+            classpathResourceMapping = Seq(
+              ("docker-initdb.d/", "/docker-entrypoint-initdb.d/", BindMode.READ_ONLY)
+            )
           )
         )
       )

08f111d066d64fd5531ec662596fb328137ed366
Bicou <bicou@bicou.com>
2020-05-05 14:23:24 +0200

    scalatest either values

--- velocypack/src/test/scala/avokka/velocypack/VPackSpecTrait.scala
+++ velocypack/src/test/scala/avokka/velocypack/VPackSpecTrait.scala
@@ -1,14 +1,13 @@
 package avokka.velocypack
 
 import org.scalatest.{Assertion, Assertions}
+import org.scalatest.EitherValues._
 
 trait VPackSpecTrait { self: Assertions =>
   def assertEnc[T](e: VPackEncoder[T], t: T, v: VPack): Assertion = {
     assertResult(v)(e.encode(t))
   }
   def assertDec[T](d: VPackDecoder[T], v: VPack, t: T): Assertion = {
-    val r = d.decode(v)
-    assert(r.isRight)
-    assertResult(t)(r.right.get)
+    assertResult(t)(d.decode(v).right.value)
   }
 }

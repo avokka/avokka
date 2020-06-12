c90149831fe7074607f6826e28d8247a9dff614d
bicou <bicou@bicou.com>
2020-06-12 13:26:57 +0200

    rename scodec applicative methods

--- fs2/src/main/scala/avokka/package.scala
+++ fs2/src/main/scala/avokka/package.scala
@@ -9,10 +9,10 @@ package object avokka {
   private def attemptEither[T](attempt: Attempt[T]): Either[Throwable, T] = attempt.toEither.leftMap(CodecException)
 
   implicit final class ApplicativeDecoderOps[T](private val decoder: Decoder[T]) {
-    def decodeA[F[_]](bits: BitVector)(implicit A: ApplicativeError[F, Throwable]): F[DecodeResult[T]] = A.fromEither(attemptEither(decoder.decode(bits)))
+    def decodeF[F[_]](bits: BitVector)(implicit A: ApplicativeError[F, Throwable]): F[DecodeResult[T]] = A.fromEither(attemptEither(decoder.decode(bits)))
   }
 
   implicit final class ApplicativeEncoderOps[T](private val encoder: Encoder[T]) {
-    def encodeA[F[_]](value: T)(implicit A: ApplicativeError[F, Throwable]): F[BitVector] = A.fromEither(attemptEither(encoder.encode(value)))
+    def encodeF[F[_]](value: T)(implicit A: ApplicativeError[F, Throwable]): F[BitVector] = A.fromEither(attemptEither(encoder.encode(value)))
   }
 }

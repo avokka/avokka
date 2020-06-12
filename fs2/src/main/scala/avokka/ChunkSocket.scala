f1ab180f63e9ed294fb8711928c77c3c56265cf4
bicou <bicou@bicou.com>
2020-06-12 13:25:10 +0200

    separate chunks in socket and messages request/response

---
+++ fs2/src/main/scala/avokka/ChunkSocket.scala
@@ -0,0 +1,118 @@
+package avokka
+
+import cats.effect.Concurrent
+import cats.effect.syntax.concurrent._
+import cats.syntax.applicative._
+import cats.syntax.flatMap._
+import cats.syntax.functor._
+import fs2.concurrent.{Queue, SignallingRef}
+import fs2.io.tcp.Socket
+import fs2.{Chunk, Pipe}
+import scodec.Codec
+import scodec.bits.ByteVector
+import scodec.codecs.{bytes, fixedSizeBytes, uint32L}
+import scodec.stream.{StreamDecoder, StreamEncoder}
+
+trait ChunkSocket[F[_]] {
+  def send(message: Long, data: ByteVector): F[Unit]
+  def pump: F[Unit]
+}
+
+object ChunkSocket {
+
+  val handshake: Chunk[Byte] = Chunk.bytes("VST/1.1\r\n\r\n".getBytes)
+
+  // 4 chunk length + 4 chunkx + 8 message id + 8 message length
+  val chunkHeaderOffset = 24
+
+  val chunkCodec: Codec[(ChunkHeader, ByteVector)] = uint32L.consume { l =>
+    ChunkHeader.codec ~ fixedSizeBytes(l - chunkHeaderOffset, bytes)
+  } { _._2.size + chunkHeaderOffset }
+
+  val chunkStreamDecoder: StreamDecoder[(ChunkHeader, ByteVector)] = StreamDecoder.many(chunkCodec)
+  val chunkStreamEncoder: StreamEncoder[(ChunkHeader, ByteVector)] = StreamEncoder.many(chunkCodec)
+
+  def apply[F[_]](
+      config: Configuration,
+      socket: Socket[F],
+      stateSignal: SignallingRef[F, ConnectionState],
+      closeSignal: SignallingRef[F, Boolean],
+      in: Pipe[F, (ChunkHeader, ByteVector), Unit],
+    )(implicit C: Concurrent[F]) = {
+
+    for {
+      requests <- Queue.unbounded[F, (ChunkHeader, ByteVector)]
+    } yield {
+
+      val chunks = requests.dequeue.evalMapChunk {
+          case (header, data) =>
+            val (chunk, tail) = data.splitAt(config.chunkLength)
+            requests.enqueue1(header.next -> tail).whenA(tail.nonEmpty).as(header -> chunk)
+        }
+
+      val outgoing: F[Unit] = chunks
+        .through(chunkStreamEncoder.toPipeByte)
+        .cons(handshake)
+        .through(socket.writes())
+        .onFinalize(stateSignal.set(ConnectionState.Disconnected))
+        .compile
+        .drain
+
+      val incoming: F[Unit] = socket
+        .reads(config.readBufferSize)
+        .through(chunkStreamDecoder.toPipeByte)
+        .through(in)
+        .onFinalize(stateSignal.set(ConnectionState.Disconnected))
+        .compile
+        .drain
+
+      val close = closeSignal.discrete
+        .evalMap(if (_) socket.close else C.unit)
+        .compile
+        .drain
+
+      new ChunkSocket[F] {
+        override def send(message: Long, data: ByteVector): F[Unit] = {
+          val chunksCount = (data.size.toDouble / config.chunkLength).ceil.toLong
+          val header = ChunkHeader(ChunkX(first = true, chunksCount), message, data.size)
+          requests.enqueue1(header -> data)
+        }
+
+        override val pump: F[Unit] =
+          for {
+            _ <- stateSignal.set(ConnectionState.Connected)
+            _ <- outgoing.race(incoming).race(close)
+          } yield ()
+      }
+    }
+  }
+
+  /*
+  val chunkSplitter: Pipe[F, (ChunkHeader, ByteVector), (ChunkHeader, ByteVector)] =
+    _.evalMapChunk {
+      case (header, data) =>
+        val (chunk, tail) = data.splitAt(config.chunkLength)
+        requests.enqueue1(header.next -> tail).whenA(tail.nonEmpty).as(header -> chunk)
+    }
+
+  val dequeueChunk: F[(ChunkHeader, ByteVector)] =
+    for {
+      (header, data) <- requests.dequeue1
+      (chunk, tail) = data.splitAt(config.chunkLength)
+      _ <- requests.enqueue1(header.next -> tail).whenA(tail.nonEmpty)
+    } yield header -> chunk
+*/
+  /*
+val chunkDecoder: Decoder[(ChunkHeader, ByteVector)] = for {
+len  <- uint32L
+header <- ChunkHeader.codec
+data <- fixedSizeBytes(len - 4 - 4 - 8 - 8, bytes)
+} yield header -> data
+
+val chunkEncoder: Encoder[(ChunkHeader, ByteVector)] = Encoder {
+case (h: ChunkHeader, b: ByteVector) => for {
+  hb <- ChunkHeader.codec.encode(h)
+} yield hb ++ b.bits
+}
+*/
+}
--- fs2/src/main/scala/avokka/Transport.scala
+++ fs2/src/main/scala/avokka/Transport.scala
@@ -2,104 +2,58 @@ package avokka
 
 import java.net.InetSocketAddress
 
-import cats.effect.concurrent.Ref
-import cats.effect.{Blocker, Concurrent, ContextShift, Resource, Timer}
-import fs2.concurrent.{Queue, SignallingRef}
-import fs2.io.tcp.{Socket, SocketGroup}
-import fs2.{Chunk, Pipe, Stream}
-import io.chrisdavenport.log4cats.Logger
-import retry.RetryDetails.{GivingUp, WillDelayAndRetry}
-import retry.{RetryDetails, RetryPolicies, retryingOnAllErrors}
-import scodec.Codec
-import scodec.bits.ByteVector
-import scodec.stream.{StreamDecoder, StreamEncoder}
-import scodec.codecs.{bytes, fixedSizeBytes, uint32L}
+import cats.effect.concurrent.{Deferred, Ref}
+import cats.effect.syntax.concurrent._
+import cats.effect.{Blocker, Concurrent, ContextShift, IO, Resource, Timer}
 import cats.syntax.apply._
 import cats.syntax.flatMap._
 import cats.syntax.functor._
-import cats.syntax.applicative._
-import cats.effect.syntax.concurrent._
+import fs2.Pipe
+import fs2.concurrent.SignallingRef
+import fs2.io.tcp.SocketGroup
+import io.chrisdavenport.log4cats.Logger
+import pureconfig.ConfigSource
+import retry.RetryDetails.{GivingUp, WillDelayAndRetry}
+import retry.{RetryDetails, RetryPolicies, retryingOnAllErrors}
+import scodec.bits.ByteVector
 
 import scala.concurrent.duration.FiniteDuration
 
 trait Transport[F[_]] {
-  def enqueueMessage(data: ByteVector): F[Unit]
+  def execute(data: ByteVector): F[ByteVector]
 }
 
 object Transport {
 
-  val handshake: Chunk[Byte] = Chunk.bytes("VST/1.1\r\n\r\n".getBytes)
-
-  // 4 chunk length + 4 chunkx + 8 message id + 8 message length
-  val chunkHeaderOffset = 24
-
-  val chunkCodec: Codec[(ChunkHeader, ByteVector)] = uint32L.consume { l =>
-    ChunkHeader.codec ~ fixedSizeBytes(l - chunkHeaderOffset, bytes)
-  } { _._2.size + chunkHeaderOffset }
-
-  val chunkStreamDecoder: StreamDecoder[(ChunkHeader, ByteVector)] = StreamDecoder.many(chunkCodec)
-  val chunkStreamEncoder: StreamEncoder[(ChunkHeader, ByteVector)] = StreamEncoder.many(chunkCodec)
-
   def apply[F[_] : ContextShift: Timer](
       config: Configuration,
-      in: Pipe[F, (ChunkHeader, ByteVector), Unit],
-      counter: Ref[F, Long],
-      requests: Queue[F, (ChunkHeader, ByteVector)],
-  //    out: Stream[F, (ChunkHeader, ByteVector)],
-      stateSignal: SignallingRef[F, ConnectionState],
-      closeSignal: SignallingRef[F, Boolean],
-  )(implicit C: Concurrent[F], L: Logger[F]): F[Transport[F]] = {
+  )(implicit C: Concurrent[F], L: Logger[F]) = {
+    for {
+      counter <- Ref.of(0L)
+      responses <- FMap[F, Long, Deferred[F, ByteVector]]
+      stateSignal <- SignallingRef[F, ConnectionState](ConnectionState.Disconnected)
+      closeSignal <- SignallingRef[F, Boolean](false)
+    } yield {
 
-    val chunkSplitter: Pipe[F, (ChunkHeader, ByteVector), (ChunkHeader, ByteVector)] =
-      _.evalMapChunk {
-        case (header, data) =>
-          val (chunk, tail) = data.splitAt(config.chunkLength)
-          requests.enqueue1(header.next -> tail).whenA(tail.nonEmpty).as(header -> chunk)
-      }
+      val in: Pipe[F, (ChunkHeader, ByteVector), Unit] = _
+        .debug()
+        .evalMapChunk {
+          case (header, vector) => responses.remove(header.message).flatMap {
+            case Some(value) => value.complete(vector)
+            case None => C.raiseError[Unit](new Exception("unknown message id"))
+          }
+        }
+        .void
 
-    /*
-    val dequeueChunk: F[(ChunkHeader, ByteVector)] =
-      for {
-        (header, data) <- requests.dequeue1
-        (chunk, tail) = data.splitAt(config.chunkLength)
-        _ <- requests.enqueue1(header.next -> tail).whenA(tail.nonEmpty)
-      } yield header -> chunk
-*/
+      val mkSocket: Resource[F, ChunkSocket[F]] = for {
+        blocker <- Blocker[F]
+        group <- SocketGroup(blocker)
+        client <- group.client(new InetSocketAddress(config.host, config.port))
+        socket <- Resource.liftF(ChunkSocket(config, client, stateSignal, closeSignal, in))
+      } yield socket
 
-    def outgoing(socket: Socket[F]): F[Unit] = requests
-        .dequeue
-        .through(chunkSplitter)
-        .through(chunkStreamEncoder.toPipeByte)
-        .cons(handshake)
-        .through(socket.writes())
-        .onFinalize(stateSignal.set(ConnectionState.Disconnected))
-        .compile
-        .drain
-
-    def incoming(socket: Socket[F]): F[Unit] = socket
-        .reads(config.readBufferSize)
-        .through(chunkStreamDecoder.toPipeByte)
-        .through(in)
-        .onFinalize(stateSignal.set(ConnectionState.Disconnected))
-        .compile
-        .drain
-
-    def close(socket: Socket[F]) =
-      closeSignal.discrete.evalMap(if (_) socket.close else C.unit).compile.drain
-
-    def pump(socket: Socket[F]) =
-      for {
-        _ <- stateSignal.set(ConnectionState.Connected)
-        _ <- outgoing(socket).race(incoming(socket)).race(close(socket))
-      } yield ()
-
-    val mkSocket: Resource[F, Socket[F]] = for {
-      blocker <- Blocker[F]
-      group <- SocketGroup(blocker)
-      client <- group.client(new InetSocketAddress(config.host, config.port))
-    } yield client
-
-    def loop(): F[Unit] = {
+      /*
+    val loop: F[Unit] = {
 
       val policy = RetryPolicies.limitRetries[F](5)
 
@@ -113,37 +67,31 @@ object Transport {
         }
 
       retryingOnAllErrors(policy, onError) {
-        mkSocket.use(pump)
+        mkSocket.use(_.pump)
       } >> stateSignal.get.flatMap {
-        case ConnectionState.Disconnected => closeSignal.set(false) >> loop()
+        case ConnectionState.Disconnected => closeSignal.set(false) >> loop
         case _                            => C.unit
       }
     }
+       */
 
-    loop().start.as(new Transport[F] {
-      override def enqueueMessage(data: ByteVector): F[Unit] = {
-        val chunksCount = (data.size.toDouble / config.chunkLength).ceil.toLong
+      mkSocket.flatMap { socket =>
 
-        for {
-          id <- counter.getAndUpdate(_ + 1)
-          header = ChunkHeader(ChunkX(first = true, chunksCount), id, data.size)
-          _ <- requests.enqueue1(header -> data)
-        } yield ()
+        val transport: Transport[F] = new Transport[F] {
+          override def execute(data: ByteVector): F[ByteVector] = for {
+            id <- counter.updateAndGet(_ + 1)
+            _ <- L.debug(s"prepare message id = $id")
+            dfr <- Deferred[F, ByteVector]
+            _ <- responses.update(id, dfr)
+            _ <- L.debug(s"queuing message id = $id")
+            _ <- socket.send(id, data)
+            r <- dfr.get
+          } yield r
+        }
+
+        socket.pump.background.as(transport)
       }
-    })
+    }
   }
 
-  /*
-val chunkDecoder: Decoder[(ChunkHeader, ByteVector)] = for {
-  len  <- uint32L
-  header <- ChunkHeader.codec
-  data <- fixedSizeBytes(len - 4 - 4 - 8 - 8, bytes)
-} yield header -> data
-
-val chunkEncoder: Encoder[(ChunkHeader, ByteVector)] = Encoder {
-  case (h: ChunkHeader, b: ByteVector) => for {
-    hb <- ChunkHeader.codec.encode(h)
-  } yield hb ++ b.bits
-}
- */
 }

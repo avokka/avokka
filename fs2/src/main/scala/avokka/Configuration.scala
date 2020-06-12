8a9368441b3190934fdafbe3bbdf3fe5d7e5dfff
bicou <bicou@bicou.com>
2020-06-12 13:26:08 +0200

    pureconfig configuration reader

--- fs2/src/main/scala/avokka/Configuration.scala
+++ fs2/src/main/scala/avokka/Configuration.scala
@@ -1,9 +1,15 @@
 package avokka
 
-final case class Configuration (
-                                 host: String = "localhost",
-                                 port: Int = 8529,
-                                 chunkLength: Long = 30000L,
-                                 readBufferSize: Int = 256 * 1024
-                               )
+import pureconfig._
+import pureconfig.generic.semiauto._
 
+final case class Configuration(
+    host: String = "localhost",
+    port: Int = 8529,
+    chunkLength: Long = 30000L,
+    readBufferSize: Int = 256 * 1024
+)
+
+object Configuration {
+  implicit val reader: ConfigReader[Configuration] = deriveReader
+}

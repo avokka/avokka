33be0df176707b1afcdaab50cd46bb4b82f4096b
bicou <bicou@bicou.com>
2021-03-11 15:57:41 +0100

    vpack long encoder refactor

--- velocypack/src/main/scala/avokka/velocypack/codecs/VPackLongCodec.scala
+++ velocypack/src/main/scala/avokka/velocypack/codecs/VPackLongCodec.scala
@@ -21,28 +21,33 @@ private[codecs] object VPackLongCodec {
   private[codecs] val encoder: Encoder[VLong] = new Encoder[VLong] {
     override def sizeBound: SizeBound = SizeBound.bounded(8 + 8, 8 + 64)
 
-    override def encode(v: VLong): Attempt[BitVector] = {
-      v.value match {
-        // negative as signed
-        case s if s < 0 && s >= -(1L << 7)  => longL(8).encode(s).map  { BitVector(0x20) ++ _ }
-        case s if s < 0 && s >= -(1L << 15) => longL(16).encode(s).map { BitVector(0x21) ++ _ }
-        case s if s < 0 && s >= -(1L << 23) => longL(24).encode(s).map { BitVector(0x22) ++ _ }
-        case s if s < 0 && s >= -(1L << 31) => longL(32).encode(s).map { BitVector(0x23) ++ _ }
-        case s if s < 0 && s >= -(1L << 39) => longL(40).encode(s).map { BitVector(0x24) ++ _ }
-        case s if s < 0 && s >= -(1L << 47) => longL(48).encode(s).map { BitVector(0x25) ++ _ }
-        case s if s < 0 && s >= -(1L << 55) => longL(56).encode(s).map { BitVector(0x26) ++ _ }
-        case s if s < 0                     => longL(64).encode(s).map { BitVector(0x27) ++ _ }
-        // positive as unsigned
-        case u if u >= 0 && u < (1L << 8) => ulongL(8).encode(u).map  { BitVector(0x28) ++ _ }
-        case u if u > 0 && u < (1L << 16) => ulongL(16).encode(u).map { BitVector(0x29) ++ _ }
-        case u if u > 0 && u < (1L << 24) => ulongL(24).encode(u).map { BitVector(0x2a) ++ _ }
-        case u if u > 0 && u < (1L << 32) => ulongL(32).encode(u).map { BitVector(0x2b) ++ _ }
-        case u if u > 0 && u < (1L << 40) => ulongL(40).encode(u).map { BitVector(0x2c) ++ _ }
-        case u if u > 0 && u < (1L << 48) => ulongL(48).encode(u).map { BitVector(0x2d) ++ _ }
-        case u if u > 0 && u < (1L << 56) => ulongL(56).encode(u).map { BitVector(0x2e) ++ _ }
-        case u if u > 0                   => longL(64).encode(u).map  { BitVector(0x2f) ++ _ }
-      }
+    // negative as signed
+    def encodeNegative(s: Long): Attempt[BitVector] = {
+      if      (s >= -(1L << 7))  longL(8).encode(s).map  { BitVector(0x20) ++ _ }
+      else if (s >= -(1L << 15)) longL(16).encode(s).map { BitVector(0x21) ++ _ }
+      else if (s >= -(1L << 23)) longL(24).encode(s).map { BitVector(0x22) ++ _ }
+      else if (s >= -(1L << 31)) longL(32).encode(s).map { BitVector(0x23) ++ _ }
+      else if (s >= -(1L << 39)) longL(40).encode(s).map { BitVector(0x24) ++ _ }
+      else if (s >= -(1L << 47)) longL(48).encode(s).map { BitVector(0x25) ++ _ }
+      else if (s >= -(1L << 55)) longL(56).encode(s).map { BitVector(0x26) ++ _ }
+      else                       longL(64).encode(s).map { BitVector(0x27) ++ _ }
     }
+
+    // positive as unsigned
+    def encodePositive(u: Long): Attempt[BitVector] = {
+      if      (u < (1L << 8))  ulongL(8).encode(u).map  { BitVector(0x28) ++ _ }
+      else if (u < (1L << 16)) ulongL(16).encode(u).map { BitVector(0x29) ++ _ }
+      else if (u < (1L << 24)) ulongL(24).encode(u).map { BitVector(0x2a) ++ _ }
+      else if (u < (1L << 32)) ulongL(32).encode(u).map { BitVector(0x2b) ++ _ }
+      else if (u < (1L << 40)) ulongL(40).encode(u).map { BitVector(0x2c) ++ _ }
+      else if (u < (1L << 48)) ulongL(48).encode(u).map { BitVector(0x2d) ++ _ }
+      else if (u < (1L << 56)) ulongL(56).encode(u).map { BitVector(0x2e) ++ _ }
+      else                     longL(64).encode(u).map  { BitVector(0x2f) ++ _ }
+    }
+
+    override def encode(v: VLong): Attempt[BitVector] =
+      if (v.value < 0) encodeNegative(v.value) else encodePositive(v.value)
+
   }
 
   private[codecs] def decoderSigned(t: IntSignedType): Decoder[VLong] =

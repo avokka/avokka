815305f0579ad1a07434026db490e922e71e430d
bicou <bicou@bicou.com>
2021-03-15 15:31:27 +0100

    index types

--- arangodb/src/main/scala/avokka/arangodb/ArangoIndexes.scala
+++ arangodb/src/main/scala/avokka/arangodb/ArangoIndexes.scala
@@ -2,6 +2,7 @@ package avokka.arangodb
 
 import api._
 import avokka.velocypack._
+import enumeratum._
 import types._
 import protocol._
 
@@ -30,41 +31,214 @@ trait ArangoIndexes[F[_]] {
     * @param sparse if true, then create a sparse index
     * @param deduplicate if false, the deduplication of array values is turned off
     */
-  def createIndexHash(
-                       fields: List[String],
-                       unique: Boolean = false,
-                       sparse: Boolean = false,
-                       deduplicate: Boolean = false,
-                     ): F[ArangoResponse[Index]]
+  def createHash(
+      fields: List[String],
+      unique: Boolean = false,
+      sparse: Boolean = false,
+      deduplicate: Boolean = false,
+  ): F[ArangoResponse[Index]]
+
+  /**
+    * Creates a skip-list index for the collection collection-name, if it does not already exist. The call expects an object containing the index details.
+    *
+    * In a sparse index all documents will be excluded from the index that do not contain at least one of the specified index attributes (i.e. fields) or that have a value of null in any of the specified index attributes. Such documents will not be indexed, and not be taken into account for uniqueness checks if the unique flag is set.
+    *
+    * In a non-sparse index, these documents will be indexed (for non-present indexed attributes, a value of null will be used) and will be taken into account for uniqueness checks if the unique flag is set.
+    *
+    * Note: unique indexes on non-shard keys are not supported in a cluster.
+    *
+    * @param fields an array of attribute paths
+    * @param unique if true, then create a unique index
+    * @param sparse if true, then create a sparse index
+    * @param deduplicate if false, the deduplication of array values is turned off
+    */
+  def createSkipList(
+      fields: List[String],
+      unique: Boolean = false,
+      sparse: Boolean = false,
+      deduplicate: Boolean = false,
+  ): F[ArangoResponse[Index]]
+
+  /**
+    * Creates a persistent index for the collection collection-name, if it does not already exist. The call expects an object containing the index details.
+    *
+    * In a sparse index all documents will be excluded from the index that do not contain at least one of the specified index attributes (i.e. fields) or that have a value of null in any of the specified index attributes. Such documents will not be indexed, and not be taken into account for uniqueness checks if the unique flag is set.
+    *
+    * In a non-sparse index, these documents will be indexed (for non-present indexed attributes, a value of null will be used) and will be taken into account for uniqueness checks if the unique flag is set.
+    *
+    * Note: unique indexes on non-shard keys are not supported in a cluster.
+    *
+    * @param fields an array of attribute paths
+    * @param unique if true, then create a unique index
+    * @param sparse if true, then create a sparse index
+    */
+  def createPersistent(
+      fields: List[String],
+      unique: Boolean = false,
+      sparse: Boolean = false,
+  ): F[ArangoResponse[Index]]
+
+  /**
+    * Creates a geo-spatial index in the collection collection-name, if it does not already exist. Expects an object containing the index details.
+    *
+    * Geo indexes are always sparse, meaning that documents that do not contain the index attributes or have non-numeric values in the index attributes will not be indexed.
+    *
+    * @param fields An array with one or two attribute paths.
+    *               If it is an array with one attribute path location, then a geo-spatial index on all documents is created using location as path to the coordinates. The value of the attribute must be an array with at least two double values. The array must contain the latitude (first value) and the longitude (second value). All documents, which do not have the attribute path or with value that are not suitable, are ignored.
+    *               If it is an array with two attribute paths latitude and longitude, then a geo-spatial index on all documents is created using latitude and longitude as paths the latitude and the longitude. The value of the attribute latitude and of the attribute longitude must a double. All documents, which do not have the attribute paths or which values are not suitable, are ignored.
+    * @param geoJson If a geo-spatial index on a location is constructed and geoJson is true, then the order within the array is longitude followed by latitude. This corresponds to the format described in http://geojson.org/geojson-spec.html#positions
+    */
+  def createGeo(
+      fields: List[String],
+      geoJson: Boolean = false,
+  ): F[ArangoResponse[Index]]
+
+  /**
+    * Creates a fulltext index for the collection collection-name, if it does not already exist. The call expects an object containing the index details.
+    *
+    * @param fields an array of attribute names. Currently, the array is limited to exactly one attribute
+    * @param minLength Minimum character length of words to index. Will default to a server-defined value if unspecified. It is thus recommended to set this value explicitly when creating the index.
+    */
+  def createFullText(
+      fields: List[String],
+      minLength: Option[Int] = None,
+  ): F[ArangoResponse[Index]]
+
+  /**
+    * Creates a TTL index for the collection collection-name if it does not already exist. The call expects an object containing the index details.
+    *
+    * @param fields an array with exactly one attribute path
+    * @param expireAfter The time (in seconds) after a document’s creation after which the documents count as “expired”.
+    */
+  def createTtl(
+      fields: List[String],
+      expireAfter: Int,
+  ): F[ArangoResponse[Index]]
 }
 
 object ArangoIndexes {
-  def apply[F[_]: ArangoClient](database: DatabaseName, collection: CollectionName): ArangoIndexes[F] = new ArangoIndexes[F] {
+  def apply[F[_]: ArangoClient](database: DatabaseName, collection: CollectionName): ArangoIndexes[F] =
+    new ArangoIndexes[F] {
 
-    override def list(): F[ArangoResponse[IndexList]] =
-      GET(
-        database,
-        "/_api/index",
-        Map("collection" -> collection.repr)
-      ).execute
+      private val api: String = "/_api/index"
 
-    override def createIndexHash(fields: List[String],
-                                 unique: Boolean,
-                                 sparse: Boolean,
-                                 deduplicate: Boolean): F[ArangoResponse[Index]] =
-      POST(
-        database,
-        "/_api/index",
-        Map("collection" -> collection.repr)
-      ).body(
-        VObject(
-          "type" -> "hash".toVPack,
-          "fields" -> fields.toVPack,
-          "unique" -> unique.toVPack,
-          "sparse" -> sparse.toVPack,
-          "deduplicate" -> deduplicate.toVPack
-        )
-      ).execute
+      override def list(): F[ArangoResponse[IndexList]] =
+        GET(
+          database,
+          api,
+          Map("collection" -> collection.repr)
+        ).execute
 
-  }
+      override def createHash(
+          fields: List[String],
+          unique: Boolean,
+          sparse: Boolean,
+          deduplicate: Boolean
+      ): F[ArangoResponse[Index]] =
+        POST(
+          database,
+          api,
+          Map("collection" -> collection.repr)
+        ).body(
+            VObject(
+              "type" -> Index.Type.hash.toVPack,
+              "fields" -> fields.toVPack,
+              "unique" -> unique.toVPack,
+              "sparse" -> sparse.toVPack,
+              "deduplicate" -> deduplicate.toVPack
+            )
+          )
+          .execute
+
+      override def createSkipList(
+          fields: List[String],
+          unique: Boolean,
+          sparse: Boolean,
+          deduplicate: Boolean
+      ): F[ArangoResponse[Index]] =
+        POST(
+          database,
+          api,
+          Map("collection" -> collection.repr)
+        ).body(
+            VObject(
+              "type" -> Index.Type.skiplist.toVPack,
+              "fields" -> fields.toVPack,
+              "unique" -> unique.toVPack,
+              "sparse" -> sparse.toVPack,
+              "deduplicate" -> deduplicate.toVPack
+            )
+          )
+          .execute
+
+      override def createPersistent(
+          fields: List[String],
+          unique: Boolean,
+          sparse: Boolean
+      ): F[ArangoResponse[Index]] =
+        POST(
+          database,
+          api,
+          Map("collection" -> collection.repr)
+        ).body(
+            VObject(
+              "type" -> Index.Type.persistent.toVPack,
+              "fields" -> fields.toVPack,
+              "unique" -> unique.toVPack,
+              "sparse" -> sparse.toVPack,
+            )
+          )
+          .execute
+
+      override def createGeo(
+          fields: List[String],
+          geoJson: Boolean,
+      ): F[ArangoResponse[Index]] =
+        POST(
+          database,
+          api,
+          Map("collection" -> collection.repr)
+        ).body(
+            VObject(
+              "type" -> Index.Type.geo.toVPack,
+              "fields" -> fields.toVPack,
+              "geoJson" -> geoJson.toVPack,
+            )
+          )
+          .execute
+
+      override def createFullText(
+          fields: List[String],
+          minLength: Option[Int],
+      ): F[ArangoResponse[Index]] =
+        POST(
+          database,
+          api,
+          Map("collection" -> collection.repr)
+        ).body(
+            VObject(
+              "type" -> Index.Type.fulltext.toVPack,
+              "fields" -> fields.toVPack,
+              "minLength" -> minLength.toVPack,
+            )
+          )
+          .execute
+
+      override def createTtl(
+          fields: List[String],
+          expireAfter: Int,
+      ): F[ArangoResponse[Index]] =
+        POST(
+          database,
+          api,
+          Map("collection" -> collection.repr)
+        ).body(
+            VObject(
+              "type" -> Index.Type.ttl.toVPack,
+              "fields" -> fields.toVPack,
+              "expireAfter" -> expireAfter.toVPack,
+            )
+          )
+          .execute
+    }
 }
--- arangodb/src/main/scala/avokka/arangodb/api/Index.scala
+++ arangodb/src/main/scala/avokka/arangodb/api/Index.scala
@@ -2,12 +2,15 @@ package avokka.arangodb
 package api
 
 import avokka.velocypack._
+import enumeratum._
+
+import _root_.enumeratum._
 
 final case class Index(
     fields: List[String],
     id: String,
     name: String,
-    `type`: String,
+    `type`: Index.Type,
     isNewlyCreated: Boolean = false,
     selectivityEstimate: Option[Double] = None,
     sparse: Option[Boolean] = None,
@@ -17,4 +20,20 @@ final case class Index(
 
 object Index {
   implicit val decoder: VPackDecoder[Index] = VPackDecoder.gen
+
+  sealed trait Type extends EnumEntry
+  object Type extends Enum[Type] {
+    override val values = findValues
+
+    case object primary extends Type
+    case object hash extends Type
+    case object skiplist extends Type
+    case object persistent extends Type
+    case object geo extends Type
+    case object geo1 extends Type
+    case object geo2 extends Type
+    case object fulltext extends Type
+    case object edge extends Type
+    case object ttl extends Type
+  }
 }

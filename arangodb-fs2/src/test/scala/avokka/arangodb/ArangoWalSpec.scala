c50da65a5bd154b3c68d74236b7f7471d08f6f51
bicou <bicou@bicou.com>
2021-06-25 11:48:53 +0200

    wal api fail with arango 3.6

--- arangodb-fs2/src/test/scala/avokka/arangodb/ArangoWalSpec.scala
+++ arangodb-fs2/src/test/scala/avokka/arangodb/ArangoWalSpec.scala
@@ -3,7 +3,7 @@ package avokka.arangodb
 class ArangoWalSpec extends ArangoIOBase {
 
   it should "wal tail" in { arango =>
-    assumeArangoVersion("3.7") // arango 3.6 fails to generate a dump response
+    assumeArangoVersion("3.7.0") // arango 3.6 fails to generate a dump response
     arango.db.wal.tail().map { res =>
       res.header.responseCode should be (200)
       res.body should not be empty
